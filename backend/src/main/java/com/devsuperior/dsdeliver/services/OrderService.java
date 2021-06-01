6518eed697f0b6e87e8c66ed75deadade7528143
abedess <lucas.abrantes.silva@ccc.ufcg.edu.br>
2021-06-01 18:05:26 -0300

    PUT orders/{id}/delivered

--- backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
@@ -45,4 +45,12 @@ public class OrderService {
 		return new OrderDTO(order);
 	}
 	
+	@Transactional
+	public OrderDTO setDelivered(Long id) {
+		Order order = orderRepository.getOne(id);
+		order.setStatus(OrderStatus.DELIVERED);
+		orderRepository.save(order);
+		return new OrderDTO(order);
+	}
+	
 }
